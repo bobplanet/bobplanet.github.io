19a4dd116271bbb8d8c09ca258f560034910431e
Jude Park <dusskapark@gmail.com>
2016-01-12 00:08:44 +0900

    카카오 링크 수정

--- assets/js/init.js
+++ assets/js/init.js
@@ -169,8 +169,8 @@ sharekakao = function(resp) {
 
   // 카카오톡 링크 버튼을 생성합니다. 처음 한번만 호출하면 됩니다.
   Kakao.Link.createTalkLinkButton({
-    container: '.kakao-link-btn',
-    label: resp.date + '오늘의 ' + resp.when + '메뉴를 알려드립니다. 메인: ' + resp.item.name + ', 서브: ' + description,
+    // container: '.kakao-link-btn',
+    label: '[' + resp.date + ']' + '오늘의 ' + resp.when + '메뉴를 알려드립니다. "' + resp.item.name + '", ' + description,
     image: {
       src: resp.item.image,
       width: '300',
